e08cfeda2825a1841c98ee71c8c7a4fe6dfda279
lisapel <lisa.peldan@gmail.com>
2022-11-24 13:04:00 +0100

    Add answers to questions

--- src/MultipleChoiceQuestion/Client/QuestionsAndAnswers.java
+++ src/MultipleChoiceQuestion/Client/QuestionsAndAnswers.java
@@ -13,7 +13,7 @@ public class QuestionsAndAnswers {
         ArrayList<JButton> list = new ArrayList<>();
         list.add(new JButton("Sport"));
         list.add(new JButton("Historia"));
-        list.add(new JButton("Matematik"));
+        list.add(new JButton("Film"));
         list.add(new JButton("Människokroppen"));
         list.add(new JButton("Java"));
         Collections.shuffle(list);
@@ -22,7 +22,7 @@ public class QuestionsAndAnswers {
     public String sportQuestions(){
         ArrayList<String>sport = new ArrayList<>();
         sport.add("Vilket lag har färgerna svart och gul i sin logga?");
-        sport.add("Vem är snabbast i världen?");
+        sport.add("Vem fick guldbollen 2021?");
         shuffleList(sport);
         return sport.get(0);
     }
@@ -33,27 +33,108 @@ public class QuestionsAndAnswers {
         shuffleList(history);
         return history.get(0);
     }
-    public String mathQuestions(){
+    public String movieQuestions(){
         ArrayList<String>math = new ArrayList<>();
-        math.add("Vad är 3*3");
-        math.add("Vilket tecken anger modulus");
+        math.add("Vilken film vann Leonardo Dicaprio en Oscar för?");
+        math.add("Från vilken film kommer citatet  \" Say hello to my little friend \"  ");
         shuffleList(math);
         return math.get(0);
     }
     public String humanBodyQuestions(){
         ArrayList<String>human=new ArrayList<>();
-        human.add("Hur många ben har en människa?");
-        human.add("Vad är det latinska ordet för kropp?");
+        human.add("Vilket är kroppens längsta ben?");
+        human.add("Vilken funktion fyller amygdala");
         shuffleList(human);
         return human.get(0);
     }
     public String javaQuestions(){
         ArrayList<String>java=new ArrayList<>();
-        java.add("Hur ser logotypen för java-språket ut?");
+        java.add("Vilket tecken anger modulus");
         java.add("Hur adderar du kommentarer till din java-kod?");
         shuffleList(java);
         return java.get(0);
 
+    }
+    public ArrayList<String> sportAnswers1(){
+        ArrayList<String> answer = new ArrayList<>();
+        answer.add("DIF");
+        answer.add("AIK");
+        answer.add("MFF");
+        answer.add("HIF");
+        return answer;
+    }
+    public ArrayList<String>sportAnswers2(){
+        ArrayList<String> answer = new ArrayList<>();
+        answer.add("Emil Forsberg");
+        answer.add("Dejan Kulusevski");
+        answer.add("Alexander Isak");
+        answer.add("Zlatan Ibrahimovic");
+        return answer;
+    }
+
+    public ArrayList<String>historyAnswers1(){
+        ArrayList<String> answer = new ArrayList<>();
+        answer.add("1912");
+        answer.add("1913");
+        answer.add("1914");
+        answer.add("1915");
+        return answer;
+    }
+    public ArrayList<String>historyAnswers2(){
+        ArrayList<String> answer = new ArrayList<>();
+        answer.add("Sköt sig själv");
+        answer.add("Blev skjuten");
+        answer.add("Blev förgiftad");
+        answer.add("Hängde sig");
+        return answer;
+    }
+    public ArrayList<String>movieAnswers1(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("The Revenant");
+        answer.add("The Wolf Of Wall Street");
+        answer.add("Shutter Island");
+        answer.add("The Great Gatsby");
+        return answer;
+    }
+    public ArrayList<String>movieAnswers2(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("Den nakna pistolen");
+        answer.add("Scarface");
+        answer.add("Stuart Little");
+        answer.add("The Big Lebowski");
+        return answer;
+    }
+    public ArrayList<String>bodyAnswers1(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("Ryggraden");
+        answer.add("Lårbenet");
+        answer.add("Skenbenet");
+        answer.add("Armbågsbenet");
+        return answer;
+    }
+    public ArrayList<String>bodyAnswers2(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("Konsekvenstänk");
+        answer.add("Logiskt tänkande");
+        answer.add("Balans och lokalsinne");
+        answer.add("Känslor");
+        return answer;
+    }
+    public ArrayList<String>javaAnswers1(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("§");
+        answer.add("%");
+        answer.add("½");
+        answer.add("?");
+        return answer;
+    }
+    public ArrayList<String>javaAnswers2(){
+        ArrayList<String>answer = new ArrayList<>();
+        answer.add("//");
+        answer.add("##");
+        answer.add("||");
+        answer.add("**");
+        return answer;
     }
     public void shuffleList(ArrayList list){
         Collections.shuffle(list);
